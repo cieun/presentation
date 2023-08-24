4f575e07e422e1c3ccb819985a2fb7fb4df9927b
cieun <129525965+cieun@users.noreply.github.com>
2023-08-24 20:43:37 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const aboutfolder = document.querySelector("#aboutfolder");
+const about = document.querySelector("#about");
+
+aboutfolder.addEventListener("click", ()=>{
+    if(about.css("display")=="none"){
+
+    }
+
+})
